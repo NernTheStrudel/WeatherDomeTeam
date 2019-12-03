7354467e99da3379b7936012604804b5b1523793
Erin's Desktop <nern.strudel@gmail.com>
2019-12-02 23:02:02 -0500

    we out here vaporwavin'

---
+++ Cloud Dome Demo/Assets/ClatterSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD;
+
+public class ClatterSound : MonoBehaviour
+{
+
+    public FMODUnity.StudioEventEmitter emitter;
+
+    public float setvalue = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            StartCoroutine(quickChange());
+        }
+    }
+
+    public IEnumerator quickChange()
+    {
+        emitter.SetParameter("Parameter 1", setvalue);
+        yield return new WaitForSeconds(0.5f);
+
+        emitter.SetParameter("Parameter 1", 0);
+    }
+}
--- Cloud Dome Demo/Assets/Rotate.cs
+++ Cloud Dome Demo/Assets/Rotate.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Rotate : MonoBehaviour
 {
 
     float speed = 5;
+
+    public Material skyMat;
+
+    public Color daySky;
+
+    public Color nightSky;
+    
+
+    float maxTime = 36;
+    float timer = 36;
+
+    bool daytime = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +29,25 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        timer -= Time.deltaTime;
+
+        if(timer < 0)
+        {
+            timer = maxTime;
+
+            if (daytime)
+            {
+                daytime = false;
+                skyMat.DOColor(nightSky, "_Tint", maxTime);
+            }
+            else
+            {
+                daytime = true;
+                skyMat.DOColor(daySky, "_Tint", maxTime);
+            }
+        }
+
         gameObject.transform.Rotate(new Vector3(speed * Time.deltaTime, 0, 0));
     }
 }
