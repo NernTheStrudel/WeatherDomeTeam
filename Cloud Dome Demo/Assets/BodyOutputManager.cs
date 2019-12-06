1c19dc2839083cbfd9d95a20a2c23d73c44dcca6
Erin's Laptop <nern.strudel@gmail.com>
2019-12-06 13:30:28 -0500

    POSTPROCESSING

--- Cloud Dome Demo/Assets/BodyOutputManager.cs
+++ Cloud Dome Demo/Assets/BodyOutputManager.cs
@@ -11,10 +11,25 @@ public class BodyOutputManager : MonoBehaviour
 
     public GameObject handPrefab;
 
+    public Dictionary<ulong, GameObject> beachTowels;
+
+    public FMODUnity.StudioEventEmitter emitter;
+
+    public float setvalue = 0.2f;
+
+    public float towelCeiling = -3;
+
+    public GameObject towelPrefab;
+
+    public Vector3 TowelOffset;
+
     // Start is called before the first frame update
     void Start()
     {
+        beachTowels = new Dictionary<ulong, GameObject>();
     }
+    
+
 
     // Update is called once per frame
     void Update()
@@ -26,7 +41,11 @@ public class BodyOutputManager : MonoBehaviour
             bool isNew = true;
             foreach(GameObject body in bodies)
             {
-                if(body.name == kvp.Value.name)
+                if(body == null)
+                {
+                    bodies.Remove(body);
+                }
+                else if(body.name == kvp.Value.name)
                 {
                     isNew = false;
                     break;
@@ -37,6 +56,49 @@ public class BodyOutputManager : MonoBehaviour
             {
                 bodies.Add(kvp.Value);
                 createNewBody(kvp.Value);
+                Debug.Log("New Body");
+            }
+        }
+
+        lookForBeachTowel();
+
+
+    }
+
+    void lookForBeachTowel()
+    {
+        //get hands/feet
+
+        foreach (KeyValuePair<ulong, GameObject> kvp in kinectData._Bodies)
+        {
+
+            //if all hands/feet are below threshold, make a new towel
+            bool rightFoot = kvp.Value.transform.Find("AnkleRight").transform.position.y <= towelCeiling;
+            bool leftFoot = kvp.Value.transform.Find("AnkleLeft").transform.position.y <= towelCeiling;
+            bool rightHand = kvp.Value.transform.Find("HandRight").transform.position.y <= towelCeiling;
+            bool leftHand = kvp.Value.transform.Find("HandLeft").transform.position.y <= towelCeiling;
+
+            if (beachTowels.ContainsKey(kvp.Key))
+            {
+                //we see if the towel needs to be toggled off
+                if(!rightFoot || !leftFoot || !leftHand || !rightHand)
+                {
+                    GameObject.Destroy(beachTowels[kvp.Key]);
+                    beachTowels.Remove(kvp.Key);
+                }
+            }
+            else
+            {
+          
+
+                if(rightFoot && leftFoot && rightHand && leftHand)
+                {
+
+
+                    GameObject newTowel = Instantiate(towelPrefab, new Vector3(kvp.Value.transform.position.x + TowelOffset.x, kvp.Value.transform.position.y + TowelOffset.y, kvp.Value.transform.position.z + TowelOffset.z), Quaternion.identity) as GameObject;
+                    beachTowels.Add(kvp.Key, newTowel);
+
+                }
             }
         }
 
@@ -56,5 +118,18 @@ public class BodyOutputManager : MonoBehaviour
         HandFollow right = Instantiate(handPrefab).GetComponent<HandFollow>();
         right.toFollow = rightHand;
 
+        
+        StartCoroutine(quickChange());
+        
+
+    }
+
+    public IEnumerator quickChange()
+    {
+        emitter.SetParameter("Parameter 1", setvalue);
+        Debug.Log(emitter.Params[0]);
+        yield return new WaitForSeconds(0.5f);
+
+        emitter.SetParameter("Parameter 1", 0);
     }
 }
---
+++ Cloud Dome Demo/Assets/LightLanterns.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightLanterns : MonoBehaviour {
+
+    public GameObject lanternFire;
+
+    public GameObject currentFire;
+
+    public Transform fireRoot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnMouseDown()
+    {
+        LightLantern();
+    }
+
+    void LightLantern()
+    {
+        if (currentFire == null)//light it
+        {
+            currentFire = Instantiate(lanternFire, fireRoot.position, new Quaternion(0,0,0,0), fireRoot);
+            
+        }
+        else // unlight it
+        {
+            Destroy(currentFire);
+        }
+
+
+
+    }
+}
